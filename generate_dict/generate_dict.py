f58cdd109a840005a349b5e733ff2c5ecc031f28
yihao <isyihao@163.com>
2020-12-08 17:13:43 +0800

    调整字典生成策略

--- generate_dict/generate_dict.py
+++ generate_dict/generate_dict.py
@@ -1,44 +1,51 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import os
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # 读取label文件
-    dictMap = {}
-    labelFile = open("./train.txt")
-    line = labelFile.readline()
-    i = 0
-    while line:
-        line = line.replace('.png ', '.png\t')
-        line = line.replace('.jpg ', '.jpg\t')
-        line = line.replace('.jpeg ', '.jpeg\t')
-        strs = str.split(line, '\t')
-        if len(strs) > 1:
-            for char in strs[1]:
-                if not (char in dictMap):
-                    dictMap[char] = True
-            i = i + 1
-        line = labelFile.readline()
-    labelFile.close()
+# 标签文件所在路径
+label_files_path = "./labels"
+# 字典文件所在路径
+dict_files_path = "./dicts"
 
-    # 读取lian-med-dict
-    labelFile = open("./lian-med-dict.txt")
-    line = labelFile.readline()
-    while line:
-        line = line.replace('\n', '')
-        if not (line in dictMap):
-            dictMap[line] = True
-        line = labelFile.readline()
-    labelFile.close()
+if __name__ == '__main__':
+
+    dictMap = {}
+    dictList = []
+
+    dict_file_names = os.listdir(dict_files_path)
+    for dict_file_name in dict_file_names:
+        if not os.path.isdir(dict_file_name):
+            dict_file = open(dict_files_path + "/" + dict_file_name)
+            line = dict_file.readline()
+            while line:
+                line = line.replace('\n', '')
+                dictList.append(line)
+                if not (line in dictMap):
+                    dictMap[line] = True
+                line = dict_file.readline()
+            dict_file.close()
+            print(dict_file_name + ' is ok!')
+
+    label_file_names = os.listdir(label_files_path)
+    for label_file_name in label_file_names:
+        if not os.path.isdir(label_file_name):
+            label_file = open(label_files_path + "/" + label_file_name)
+            line = label_file.readline()
+            i = 0
+            while line:
+                strings = str.split(line, '\t')
+                if len(strings) > 1:
+                    for char in strings[1]:
+                        if not (char in dictMap):
+                            dictList.append(char)
+                            dictMap[char] = True
+                    i = i + 1
+                line = label_file.readline()
+            label_file.close()
+            print(label_file_name + ' is ok!')
+
 
     fileName = 'dict.txt'
-    with open(fileName, 'w') as f:  # 如果filename不存在会自动创建， 'w'表示写数据，写之前会清空文件中的原有数据！
-        dictMapSorted = sorted(dictMap.keys())
-        for item in dictMapSorted:
+    with open(fileName, 'w') as f:
+        #dictMapSorted = sorted(dictMap.keys())
+        for item in dictList:
             content = item + '\n'
             f.write(content)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
